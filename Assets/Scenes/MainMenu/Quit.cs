68990ef2eaefec41afb0971e9923245d33d526c3
Romez1990 <Romez1990@yandex.ru>
2018-10-03 08:57:54 +0300

    Aligned the buttons in the main menu and created a script to quit

---
+++ Assets/Scenes/MainMenu/Quit.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Quit : MonoBehaviour {
+
+	private void onClickQuit () {
+		Application.Quit();
+	}
+
+}
