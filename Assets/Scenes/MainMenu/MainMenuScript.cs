6738b65cec226e16a43abd9a73e5d90cdfc31d75
Romez1990 <Romez1990@yandex.ru>
2018-10-08 08:37:15 +0300

    Finished the moving selection through pointers and unsafe methods

--- Assets/Scenes/MainMenu/MainMenuScript.cs
+++ Assets/Scenes/MainMenu/MainMenuScript.cs
@@ -1,90 +1,70 @@
 ﻿using JetBrains.Annotations;
-using System;
-using System.Threading;
-using System.Threading.Tasks;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class MainMenuScript : MonoBehaviour {
+namespace Assets.Scenes.MainMenu {
+	public class MainMenuScript : MonoBehaviour {
 
-	[UsedImplicitly]
-	private void Start () {
-		Debug.Log(nameof(Start));
-	}
-
-	[UsedImplicitly]
-	private void Update () {
-		checkKeyToMoveOn();
-	}
-
-	#region Moving selection
-
-	bool down;
-	bool up;
-
-	struct Move {
-		public Move (ref bool _b, Action _method) {
-			b = _b;
-			method = _method;
+		[UsedImplicitly]
+		private void Start () {
+			intializingRepeators();
 		}
 
-		public bool b;
-		public Action method;
-	}
-
-	private void checkKeyToMoveOn () {
-
-		bool downDown = Input.GetKeyDown(KeyCode.DownArrow);
-		down = Input.GetKey(KeyCode.DownArrow);
-
-		if (downDown) {
-			moveSelectionDown();
-
-			Thread check = new Thread(checkHold);
-			Move m = new Move(ref down, moveSelectionDown);
-			object obj = m;
-			check.Start(obj);
+		[UsedImplicitly]
+		private void Update () {
+			checkKeyToMoveSelection();
 		}
 
-		bool upDown = Input.GetKeyDown(KeyCode.UpArrow);
-		up = Input.GetKey(KeyCode.UpArrow);
+		#region Moving selection
 
-		if (upDown) {
-			moveSelectionUp();
+		private unsafe void intializingRepeators () {
+			fixed (bool* b = &down) { repeaterDown = new Repeater(b, 450, 75); }
+			repeaterDown.Act += moveSelectionDown;
+			fixed (bool* b = &up) { repeaterUp = new Repeater(b, 450, 75); }
+			repeaterUp.Act += moveSelectionUp;
 		}
-	}
 
-	private void checkHold (object str) {
-		Move move = (Move)str;
+		Repeater repeaterDown;
+		Repeater repeaterUp;
 
-		Thread.Sleep(300);
+		bool down;
+		bool up;
 
-		while (move.b) {
-			move.method();
-			Thread.Sleep(50);
+		private void checkKeyToMoveSelection () {
+			bool downDown = Input.GetKeyDown(KeyCode.DownArrow);
+			down = Input.GetKey(KeyCode.DownArrow);
+			if (downDown) {
+				repeaterDown.startWork();
+			}
+
+			bool upDown = Input.GetKeyDown(KeyCode.UpArrow);
+			up = Input.GetKey(KeyCode.UpArrow);
+			if (upDown) {
+				repeaterUp.startWork();
+			}
 		}
+
+		private void moveSelectionDown () {
+			Debug.Log("Moved down");
+		}
+
+		private void moveSelectionUp () {
+			Debug.Log("Moved up");
+		}
+
+		#endregion
+
+		public void onClickPlay () {
+			SceneManager.LoadScene("MixedMode");
+		}
+
+		public void onClickSettings () {
+			SceneManager.LoadScene("Settings");
+		}
+
+		public void onClickQuit () {
+			Application.Quit();
+		}
+
 	}
-
-	private void moveSelectionDown () {
-		Debug.Log("Moved down");
-	}
-
-	private void moveSelectionUp () {
-		Debug.Log("Moved up");
-	}
-
-	#endregion
-
-	public void onClickPlay () {
-		SceneManager.LoadScene("MixedMode");
-	}
-
-	public void onClickSettings () {
-		SceneManager.LoadScene("Settings");
-	}
-
-	public void onClickQuit () {
-		Application.Quit();
-	}
-
 }
---
+++ Assets/Scenes/Repeater.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Assets.Scenes {
+	internal class Repeater {
+		public unsafe Repeater (bool* c, int FirstDelay, int TheRestDelay) {
+			Condition = c;
+			this.FirstDelay = FirstDelay;
+			this.TheRestDelay = TheRestDelay;
+		}
+
+		public unsafe bool* Condition { get; set; }
+		public int FirstDelay { get; set; }
+		public int TheRestDelay { get; set; }
+
+		public async void startWork () {
+			await Task.Run(() => looping());
+		}
+
+		public event Action Act;
+
+		private unsafe void looping () {
+			Act?.Invoke();
+			Thread.Sleep(FirstDelay);
+			while (*Condition) {
+				Act?.Invoke();
+				Thread.Sleep(TheRestDelay);
+			}
+		}
+	}
+}
