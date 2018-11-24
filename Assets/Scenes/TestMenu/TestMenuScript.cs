625d236c41625a2363b411dbe48d5b39cba02413
Romez <Romez1990@yandex.ru>
2018-11-24 19:09:48 +0300

    Start testing button animation on TestMenu scene

---
+++ Assets/Scenes/TestMenu/TestMenuScript.cs
@@ -0,0 +1,46 @@
+﻿using JetBrains.Annotations;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace Assets.Scenes.Test {
+	public class TestMenuScript : MonoBehaviour {
+
+		[UsedImplicitly]
+		private void Start() {
+			IntializingPanels();
+		}
+
+		#region Buttons and panels
+
+		private GameObject[] Panels;
+		private List<GameObject>[] Buttons;
+
+		private void IntializingPanels() {
+			Panels = new GameObject[transform.childCount];
+			Buttons = new List<GameObject>[Panels.Length];
+
+			for (int i = 0; i < Panels.Length; i++) {
+				Panels[i] = transform.GetChild(i).gameObject;
+				Buttons[i] = new List<GameObject>();
+
+				for (int j = 0; j < Panels[i].transform.childCount; j++) {
+					Buttons[i].Add(Panels[i].transform.GetChild(j).gameObject);
+					AddEvents(Buttons[i][j]);
+				}
+			}
+		}
+
+		private void AddEvents(GameObject Button) {
+			EventTrigger.Entry entry = new EventTrigger.Entry {
+				eventID = EventTriggerType.PointerEnter
+			};
+			entry.callback.AddListener(eventData =>
+				EventSystem.current.GetComponent<EventSystem>().SetSelectedGameObject(Button));
+			Button.AddComponent<EventTrigger>().triggers.Add(entry);
+		}
+
+		#endregion
+
+	}
+}
