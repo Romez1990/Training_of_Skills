103b5a28ae2cee45191eb682422d0d98ae94621b
Romez <Romez1990@yandex.ru>
2018-12-09 14:44:55 +0300

    Create Reaction scene and script for it

---
+++ Assets/Scenes/Games/Reaction/ReactionScript.cs
@@ -0,0 +1,18 @@
+﻿using JetBrains.Annotations;
+using UnityEngine;
+
+namespace Assets.Scenes.Games.Reaction {
+	public class ReactionScript : MonoBehaviour {
+
+		[UsedImplicitly]
+		private void Start() {
+
+		}
+
+		[UsedImplicitly]
+		private void Update() {
+
+		}
+
+	}
+}
