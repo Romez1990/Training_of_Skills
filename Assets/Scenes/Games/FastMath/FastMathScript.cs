210fecd3eecd865e2285b5f08387343b6ab5665e
Romez1990 <Romez1990@yandex.ru>
2018-12-12 08:01:22 +0300

    Make generate random number to calculate in the fast math

--- Assets/Scenes/Games/FastMath/FastMathScript.cs
+++ Assets/Scenes/Games/FastMath/FastMathScript.cs
@@ -1,5 +1,6 @@
 ﻿using Assets.Scenes.Games.BaseGame;
 using JetBrains.Annotations;
+using System.Collections;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
@@ -19,9 +20,19 @@ namespace Assets.Scenes.Games.FastMath {
 				Mathf.RoundToInt(0.7f * Mathf.Pow(1.5f, Mathf.Pow(PlayingInfo.Score, 0.3f))),
 				Mathf.RoundToInt(0.6f * Mathf.Pow(1.5f, Mathf.Pow(PlayingInfo.Score * 0.48f, 0.33f)))
 			};
+
+			//SignsProbability = new[] { 0, 0, 0, 1 };
+
 			ExpressionElements.Operation = GetSign();
 			ExpressionElements = SetNumbers();
 			Expression.text = GetExpression();
+			//StartCoroutine(Log());
+		}
+
+		private IEnumerator Log() {
+			//Debug.Log(new string('\t', 5) + $"{SignsProbability[0]}\t{SignsProbability[1]}\t{SignsProbability[2]}\t{SignsProbability[3]}\t\t{PlayingInfo.Score}");
+			yield return new WaitForSeconds(2);
+			Functions.Win();
 		}
 
 		public Text Expression;
@@ -45,37 +56,79 @@ namespace Assets.Scenes.Games.FastMath {
 		}
 
 		private static (int, char, int, int) SetNumbers() {
-			int a, b, answer;
+			int a, b, answer, min, max, min_min, max_min;
+			max = Mathf.RoundToInt(Mathf.Pow(1.5f, Mathf.Pow(PlayingInfo.Score, 0.33f)));
+			min = Mathf.RoundToInt(Mathf.Pow(1.1f, Mathf.Pow(PlayingInfo.Score, 0.485f)));
 			switch (ExpressionElements.Operation) {
 				case '+':
-					a = Random.Range(20, 51);
-					b = Random.Range(20, 51);
+					min = min < 2 ? 2 : min;
+					min = min > 499 ? 499 : min;
+					max = max < 10 ? 10 : max;
+					max = max > 499 ? 499 : max;
+
+					a = Random.Range(min, max);
+					b = Random.Range(min, max);
 					answer = a + b;
 					break;
 
 				case '-':
-					a = Random.Range(20, 100);
-					b = Random.Range(20, a + 1);
+					max *= 2;
+
+					min = min < 2 ? 2 : min;
+					min = min > 799 ? 799 : min;
+					max = max < 10 ? 10 : max;
+					max = max > 999 ? 999 : max;
+
+					a = Random.Range(min, max);
+					b = Random.Range(min, a - 1);
 					answer = a - b;
 					break;
 
 				case '×':
-					a = Random.Range(5, 21);
-					b = Random.Range(5, 21);
+					min /= 100;
+					max /= 100;
+
+					min_min = 2;
+					max_min = 5;
+					min += min_min;
+					max += max_min;
+
+					min = min < min_min ? min_min : min;
+					min = min > 20 ? 20 : min;
+					max = max < max_min ? max_min : max;
+					max = max > 31 ? 31 : max;
+
+					a = Random.Range(min, max);
+					b = Random.Range(min, max);
 					answer = a * b;
 					break;
 
-				case '÷':
-					int c = Random.Range(5, 13);
-					b = Random.Range(5, 13);
+				default: // '÷'
+					min /= 300;
+					max /= 300;
+
+					min_min = 2;
+					max_min = 5;
+					min += min_min;
+					max += max_min;
+
+					min = min < min_min ? min_min : min;
+					max = max < max_min ? max_min : max;
+					if (PlayingInfo.Score < 20_000) {
+						min = min > 10 ? 10 : min;
+						max = max > 20 ? 20 : max;
+					} else {
+						min = min > 20 ? 20 : min;
+						max = max > 31 ? 31 : max;
+					}
+
+					int c = Random.Range(min, max);
+					b = Random.Range(min, max);
 					a = b * c;
 					answer = a / b;
 					break;
-
-				default:
-					a = b = answer = 0; // But this never will happen
-					break;
 			}
+			//Debug.Log(new string('\t', 5) + $"{min}\t{max}");
 			return (a, ExpressionElements.Operation, b, answer);
 		}
 
