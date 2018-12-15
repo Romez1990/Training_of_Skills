14bbb1e6823839785847598661d78470f9d5a779
Romez1990 <Romez1990@yandex.ru>
2018-12-15 08:43:33 +0300

    Move FastMathScript functionality to its own class MathExp

--- Assets/Scenes/Games/FastMath/FastMathScript.cs
+++ Assets/Scenes/Games/FastMath/FastMathScript.cs
@@ -10,9 +10,11 @@ namespace Assets.Scenes.Games.FastMath {
 
 		#region Start
 
+		private static readonly MathExp MathExp = new MathExp();
+
 		[UsedImplicitly]
 		private void Start() {
-			SignsProbability = new[] {
+			MathExp.SignProbabilities = new[] {
 				Mathf.RoundToInt(170 / Mathf.Pow(PlayingInfo.Score / 300 + 2, 0.7f)),
 				PlayingInfo.Score < 2000 ?
 					Mathf.RoundToInt(150 / Mathf.Pow(PlayingInfo.Score / 300 + 2, 0.8f)) + 8 :
@@ -20,119 +22,24 @@ namespace Assets.Scenes.Games.FastMath {
 				Mathf.RoundToInt(0.7f * Mathf.Pow(1.5f, Mathf.Pow(PlayingInfo.Score, 0.3f))),
 				Mathf.RoundToInt(0.6f * Mathf.Pow(1.5f, Mathf.Pow(PlayingInfo.Score * 0.48f, 0.33f)))
 			};
-
-			//SignsProbability = new[] { 0, 0, 0, 1 };
-
-			ExpressionElements.Operation = GetSign();
-			ExpressionElements = SetNumbers();
-			Expression.text = GetExpression();
+			//MathExp.SignProbabilities = new[] { 0, 1, 0, 0 };
+			MathExp.SetSign();
+			MathExp.SetNumbers();
+			Expression.text = MathExp.ToString();
 			//StartCoroutine(Log());
 		}
 
 		private IEnumerator Log() {
-			//Debug.Log(new string('\t', 5) + $"{SignsProbability[0]}\t{SignsProbability[1]}\t{SignsProbability[2]}\t{SignsProbability[3]}\t\t{PlayingInfo.Score}");
-			yield return new WaitForSeconds(2);
+			//Debug.Log(new string('\t', 5) + $"{SignProbabilities[0]}\t{SignProbabilities[1]}\t{SignProbabilities[2]}\t{SignProbabilities[3]}\t\t{PlayingInfo.Score}");
+			yield return new WaitForSeconds(1);
 			Functions.Win();
 		}
 
 		public Text Expression;
-		private static (int FirstNumber, char Operation, int SecondNumber, int Answer) ExpressionElements;
 		public InputField UserAnswer;
 		public Image Indicator;
 		public Sprite TrueIndicator;
 		public Sprite FalseIndicator;
-
-		#region Get expression
-
-		private static string GetExpression() {
-			return $"{ExpressionElements.FirstNumber} {ExpressionElements.Operation} {ExpressionElements.SecondNumber} = ";
-		}
-
-		private static readonly char[] Signs = { '+', '-', '×', '÷' };
-		private int[] SignsProbability;
-
-		private char GetSign() {
-			return Signs[DistributedProbability.RandomByProbability(SignsProbability)];
-		}
-
-		private static (int, char, int, int) SetNumbers() {
-			int a, b, answer, min, max, min_min, max_min;
-			max = Mathf.RoundToInt(Mathf.Pow(1.5f, Mathf.Pow(PlayingInfo.Score, 0.33f)));
-			min = Mathf.RoundToInt(Mathf.Pow(1.1f, Mathf.Pow(PlayingInfo.Score, 0.485f)));
-			switch (ExpressionElements.Operation) {
-				case '+':
-					min = min < 2 ? 2 : min;
-					min = min > 499 ? 499 : min;
-					max = max < 10 ? 10 : max;
-					max = max > 499 ? 499 : max;
-
-					a = Random.Range(min, max);
-					b = Random.Range(min, max);
-					answer = a + b;
-					break;
-
-				case '-':
-					max *= 2;
-
-					min = min < 2 ? 2 : min;
-					min = min > 799 ? 799 : min;
-					max = max < 10 ? 10 : max;
-					max = max > 999 ? 999 : max;
-
-					a = Random.Range(min, max);
-					b = Random.Range(min, a - 1);
-					answer = a - b;
-					break;
-
-				case '×':
-					min /= 100;
-					max /= 100;
-
-					min_min = 2;
-					max_min = 5;
-					min += min_min;
-					max += max_min;
-
-					min = min < min_min ? min_min : min;
-					min = min > 20 ? 20 : min;
-					max = max < max_min ? max_min : max;
-					max = max > 31 ? 31 : max;
-
-					a = Random.Range(min, max);
-					b = Random.Range(min, max);
-					answer = a * b;
-					break;
-
-				default: // '÷'
-					min /= 300;
-					max /= 300;
-
-					min_min = 2;
-					max_min = 5;
-					min += min_min;
-					max += max_min;
-
-					min = min < min_min ? min_min : min;
-					max = max < max_min ? max_min : max;
-					if (PlayingInfo.Score < 20_000) {
-						min = min > 10 ? 10 : min;
-						max = max > 20 ? 20 : max;
-					} else {
-						min = min > 20 ? 20 : min;
-						max = max > 31 ? 31 : max;
-					}
-
-					int c = Random.Range(min, max);
-					b = Random.Range(min, max);
-					a = b * c;
-					answer = a / b;
-					break;
-			}
-			//Debug.Log(new string('\t', 5) + $"{min}\t{max}");
-			return (a, ExpressionElements.Operation, b, answer);
-		}
-
-		#endregion
 
 		#endregion
 
@@ -160,11 +67,11 @@ namespace Assets.Scenes.Games.FastMath {
 				return;
 			}
 
-			if (int.Parse(UserAnswer.text) != ExpressionElements.Answer) {
+			if (int.Parse(UserAnswer.text) != MathExp.Answer) {
 				Indicator.sprite = FalseIndicator;
-				if (UserAnswer.text.Length == ExpressionElements.Answer.ToString().Length)
+				if (UserAnswer.text.Length == MathExp.Answer.ToString().Length)
 					TimeControl.TakeTime(4);
-				else if (UserAnswer.text.Length > ExpressionElements.Answer.ToString().Length)
+				else if (UserAnswer.text.Length > MathExp.Answer.ToString().Length)
 					TimeControl.TakeTime(7);
 			} else {
 				Indicator.sprite = TrueIndicator;
---
+++ Assets/Scenes/Games/FastMath/MathExp.cs
@@ -0,0 +1,102 @@
+﻿using Assets.Scenes.Games.BaseGame;
+using UnityEngine;
+
+namespace Assets.Scenes.Games.FastMath {
+	public class MathExp {
+
+		public int[] SignProbabilities { get; set; }
+		private static readonly char[] Signs = { '+', '-', '×', '÷' };
+
+		public int Number1 { get; set; }
+		public char Sign { get; set; }
+		public int Number2 { get; set; }
+		public int Answer { get; set; }
+
+		public char SetSign() {
+			return Sign = Signs[DistributedProbability.RandomByProbability(SignProbabilities)];
+
+		}
+
+		public void SetNumbers() {
+			int min = Mathf.RoundToInt(Mathf.Pow(1.5f, Mathf.Pow(PlayingInfo.Score, 0.33f))),
+				 max = Mathf.RoundToInt(Mathf.Pow(1.1f, Mathf.Pow(PlayingInfo.Score, 0.485f))),
+				 min_min,
+				 max_min;
+
+			switch (Sign) {
+				case '+':
+					min = min < 2 ? 2 : min;
+					min = min > 499 ? 499 : min;
+					max = max < 10 ? 10 : max;
+					max = max > 499 ? 499 : max;
+
+					Number1 = Random.Range(min, max);
+					Number2 = Random.Range(min, max);
+					Answer = Number1 + Number2;
+					break;
+
+				case '-':
+					max *= 2;
+
+					min = min < 2 ? 2 : min;
+					min = min > 799 ? 799 : min;
+					max = max < 10 ? 10 : max;
+					max = max > 999 ? 999 : max;
+
+					Number1 = Random.Range(min, max);
+					Number2 = Random.Range(min, Number1 - 3);
+					Answer = Number1 - Number2;
+					break;
+
+				case '×':
+					min /= 100;
+					max /= 100;
+
+					min_min = 2;
+					max_min = 5;
+					min += min_min;
+					max += max_min;
+
+					min = min < min_min ? min_min : min;
+					min = min > 20 ? 20 : min;
+					max = max < max_min ? max_min : max;
+					max = max > 31 ? 31 : max;
+
+					Number1 = Random.Range(min, max);
+					Number2 = Random.Range(min, max);
+					Answer = Number1 * Number2;
+					break;
+
+				default: // '÷'
+					min /= 300;
+					max /= 300;
+
+					min_min = 2;
+					max_min = 5;
+					min += min_min;
+					max += max_min;
+
+					min = min < min_min ? min_min : min;
+					max = max < max_min ? max_min : max;
+					if (PlayingInfo.Score < 20_000) {
+						min = min > 10 ? 10 : min;
+						max = max > 20 ? 20 : max;
+					} else {
+						min = min > 20 ? 20 : min;
+						max = max > 31 ? 31 : max;
+					}
+
+					int c = Random.Range(min, max);
+					Number2 = Random.Range(min, max);
+					Number1 = Number2 * c;
+					Answer = Number1 / Number2;
+					break;
+			}
+		}
+
+		public override string ToString() {
+			return $"{Number1} {Sign} {Number2} = ";
+		}
+
+	}
+}
