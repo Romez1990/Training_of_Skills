4a687ff03eb4bef57edaf8e1b7ccd0cb482ebe1b
Romez1990 <Romez1990@yandex.ru>
2018-12-15 09:54:27 +0300

    Make square pause button in any screens

---
+++ Assets/Scenes/Games/BaseGame/PauseButtonResizer.cs
@@ -0,0 +1,14 @@
+﻿using JetBrains.Annotations;
+using UnityEngine;
+
+namespace Assets.Scenes.Games.BaseGame {
+	public class PauseButtonResizer : MonoBehaviour {
+
+		[UsedImplicitly]
+		private void Start() {
+			RectTransform RectTransform = GetComponent<RectTransform>();
+			RectTransform.offsetMax = new Vector2(RectTransform.rect.height, 0);
+		}
+
+	}
+}
