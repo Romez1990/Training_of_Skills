346a388f5aa4974e8d0f3a52adbce3b6eca19de3
Romez1990 <Romez1990@yandex.ru>
2018-12-16 08:57:05 +0300

    Some refactoring in the FastMath

--- Assets/Scenes/Games/FastMath/FastMathScript.cs
+++ Assets/Scenes/Games/FastMath/FastMathScript.cs
@@ -14,6 +14,23 @@ namespace Assets.Scenes.Games.FastMath {
 
 		[UsedImplicitly]
 		private void Start() {
+			Expression = transform.GetChild(0).GetComponent<Text>();
+			UserAnswer = transform.GetChild(1).GetComponent<InputField>();
+			UserAnswer.onValueChanged.AddListener(delegate { OnTextChanged(); });
+			Indicator = transform.GetChild(2).GetComponent<Image>();
+			SetExp();
+			//StartCoroutine(Log());
+		}
+
+		private IEnumerator Log() {
+			//Debug.Log(new string('\t', 5) + $"{SignProbabilities[0]}\t{SignProbabilities[1]}\t{SignProbabilities[2]}\t{SignProbabilities[3]}\t\t{PlayingInfo.Score}");
+			yield return new WaitForSeconds(1);
+			Functions.Win();
+		}
+
+		private Text Expression;
+
+		private void SetExp() {
 			MathExp.SignProbabilities = new[] {
 				Mathf.RoundToInt(170 / Mathf.Pow(PlayingInfo.Score / 300 + 2, 0.7f)),
 				PlayingInfo.Score < 2000 ?
@@ -26,24 +43,13 @@ namespace Assets.Scenes.Games.FastMath {
 			MathExp.SetSign();
 			MathExp.SetNumbers();
 			Expression.text = MathExp.ToString();
-			//StartCoroutine(Log());
 		}
-
-		private IEnumerator Log() {
-			//Debug.Log(new string('\t', 5) + $"{SignProbabilities[0]}\t{SignProbabilities[1]}\t{SignProbabilities[2]}\t{SignProbabilities[3]}\t\t{PlayingInfo.Score}");
-			yield return new WaitForSeconds(1);
-			Functions.Win();
-		}
-
-		public Text Expression;
-		public InputField UserAnswer;
-		public Image Indicator;
-		public Sprite TrueIndicator;
-		public Sprite FalseIndicator;
 
 		#endregion
 
 		#region Update
+
+		private InputField UserAnswer;
 
 		[UsedImplicitly]
 		private void Update() {
@@ -56,6 +62,10 @@ namespace Assets.Scenes.Games.FastMath {
 		#endregion
 
 		#region Check answer
+
+		private Image Indicator;
+		public Sprite TrueIndicator;
+		public Sprite FalseIndicator;
 
 		[UsedImplicitly]
 		public void OnTextChanged() {
