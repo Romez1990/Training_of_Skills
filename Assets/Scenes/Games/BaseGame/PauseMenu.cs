d47038428f2ac531be17590529a46fe46f90990c
Romez1990 <Romez1990@yandex.ru>
2018-12-16 08:26:52 +0300

    Remake PauseMenu logic
    Add buttons from scene to array instead create from prefab

--- Assets/Scenes/Games/BaseGame/PauseMenu.cs
+++ Assets/Scenes/Games/BaseGame/PauseMenu.cs
@@ -1,9 +1,6 @@
-﻿using Assets.Scenes.MainMenu;
-using JetBrains.Annotations;
-using System;
+﻿using JetBrains.Annotations;
 using UnityEngine;
 using UnityEngine.EventSystems;
-using UnityEngine.UI;
 
 namespace Assets.Scenes.Games.BaseGame {
 	public class PauseMenu : MonoBehaviour {
@@ -16,90 +13,49 @@ namespace Assets.Scenes.Games.BaseGame {
 			AddEventsToButtons();
 		}
 
-		private static readonly string[] ButtonNames = {
-			"Resume",
-			"Restart",
-			"MainMenu",
-			"Quit"
-		};
-
 		#region Initialization panels and buttons
 
-		public GameObject ButtonPrefab;
 		public static GameObject[] Buttons;
 
 		private void InitializeMenuButtons() {
-			// Remove all existing buttons from the scene
-			/*for (int i = 0; i < transform.childCount; ++i)
-				Destroy(transform.GetChild(i).gameObject);//*/
+			Buttons = new GameObject[transform.childCount];
 
-			Buttons = new GameObject[ButtonNames.Length];
 			// Set panel position and size
-			float ScreenWidth = GetComponent<RectTransform>().rect.width;
+			/*float ScreenWidth = GetComponent<RectTransform>().rect.width;
 			RectTransform PanelRectTransform = GetComponent<RectTransform>();
 			float PanelWidth = PanelRectTransform.rect.height / ButtonNames.Length * 0.82f * 4.2f;
 			PanelRectTransform.anchorMin = new Vector2((ScreenWidth - PanelWidth) / 2 / ScreenWidth, PanelRectTransform.anchorMin.y);
-			PanelRectTransform.anchorMax = new Vector2(1 - (ScreenWidth - PanelWidth) / 2 / ScreenWidth, PanelRectTransform.anchorMax.y);
+			PanelRectTransform.anchorMax = new Vector2(1 - (ScreenWidth - PanelWidth) / 2 / ScreenWidth, PanelRectTransform.anchorMax.y);//*/
 
 			// Button creating
 			for (int i = 0; i < Buttons.Length; ++i) {
-				GameObject ButtonWrapper = Instantiate(ButtonPrefab, transform);
-				ButtonWrapper.name = ButtonNames[i] + "Wrapper";
+				GameObject ButtonWrapper = transform.GetChild(i).gameObject;
 
 				// Set position and size
-				RectTransform ButtonRectTransform = ButtonWrapper.GetComponent<RectTransform>();
+				/*RectTransform ButtonRectTransform = ButtonWrapper.GetComponent<RectTransform>();
 				ButtonRectTransform.anchorMin = new Vector2(0, (ButtonNames.Length - i - 1) / (float)ButtonNames.Length);
-				ButtonRectTransform.anchorMax = new Vector2(1, (ButtonNames.Length - i) / (float)ButtonNames.Length);
+				ButtonRectTransform.anchorMax = new Vector2(1, (ButtonNames.Length - i) / (float)ButtonNames.Length);//*/
 
 				Buttons[i] = ButtonWrapper.transform.GetChild(0).gameObject;
-				Buttons[i].name = ButtonNames[i];
-
-				// Set text
-				Buttons[i].transform.GetChild(0).GetComponent<Text>().text = ButtonNames[i].ToNormalCase();
 			}
 		}
 
 		#endregion
 
-		#region Clicks
+		#region Events
 
 		public static void AddEventsToButtons() {
 			foreach (GameObject Button in Buttons) {
-				// Add mouse over event
 				EventTrigger.Entry Entry = new EventTrigger.Entry { eventID = EventTriggerType.PointerEnter };
 				Entry.callback.AddListener(delegate { EventSystem.current.SetSelectedGameObject(Button); });
 				Button.AddComponent<EventTrigger>().triggers.Add(Entry);
-
-				// Add click event
-				Button.GetComponent<Button>().onClick.AddListener(delegate { OnButtonClick(Button.name); });
 			}
 		}
 
-		public struct ButtonClick {
-			public readonly string Name;
-			public readonly Action OnClick;
-
-			public ButtonClick(string Name, Action OnClick) {
-				this.Name = Name;
-				this.OnClick = OnClick;
-			}
-		}
-
-		private static readonly ButtonClick[] ButtonClicks = {
-			new ButtonClick(ButtonNames[0], delegate { PauseControl.IsPause = false; }),
-			new ButtonClick(ButtonNames[1], Functions.ReloadGame),
-			new ButtonClick(ButtonNames[2], delegate { Functions.LoadGame("MainMenu"); }),
-			new ButtonClick(ButtonNames[3], Application.Quit)
-		};
-
-		public static void OnButtonClick(string ButtonName) {
-			foreach (ButtonClick ButtonEvent in ButtonClicks) {
-				if (ButtonEvent.Name == ButtonName) {
-					ButtonEvent.OnClick();
-					return;
-				}
-			}
-		}
+		[UsedImplicitly] public void ClickResume() => PauseControl.IsPause = false;
+		[UsedImplicitly] public void ClickREstart() => Functions.ReloadGame();
+		[UsedImplicitly] public void ClickMainMenu() => Functions.LoadGame("MainMenu");
+		[UsedImplicitly] public void ClickQuit() => Application.Quit();
 
 		#endregion
 
